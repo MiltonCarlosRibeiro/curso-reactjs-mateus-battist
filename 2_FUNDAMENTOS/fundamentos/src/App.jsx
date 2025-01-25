d02b611f265a2eb4e27905a3010699d0ab80592b
Milton Carlos Ribeiro <147319237+MiltonCarlosRibeiro@users.noreply.github.com>
2025-01-25 23:19:20 +0900

    Importando componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-
+import FirstComponent from './components/FirstComponent'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
 
     </div>
   )
